15ecc32079413ad87b618a9b4650c9a74feca8ab
uventura <ualivent@gmail.com>
2022-12-17 12:54:10 -0300

    Fixing around function in python script

--- lab01/main.py
+++ lab01/main.py
@@ -7,7 +7,7 @@ def print_board(board):
 def dir_increment(board, symbol, row, col, dir_row, dir_col):
     if not(0 <= row + dir_row < len(board)
     and    0 <= col + dir_col < len(board[0])):
-            return 0
+            return -1
     count = 0
     while board[row + dir_row][col + dir_col] == symbol:
         row += dir_row
@@ -19,17 +19,35 @@ def dir_increment(board, symbol, row, col, dir_row, dir_col):
     return count
 
 def around(board, symbol, row, col):
-    south = dir_increment(board, symbol, row, col, 1, 0)
-     = dir_increment(board, symbol, row, col, 0, 1)
-    right = dir_increment(board, symbol, row, col, 0, -1)
-    diag_1 = dir_increment(board, symbol, row, col, )
+    '''
+               (N)
+            NW  ^  NE
+        (W) <       > (E)
+            SW  v  SE
+               (S)
+        N => North
+        W => West
+        E => East
+        S => South
+    '''
+    # Don't need to analyze north
+    directions = {
+        'W':  [ 0, -1],
+        'E':  [ 0,  1],
+        'S':  [-1,  0],
+        'NW': [ 1, -1],
+        'NE': [ 1,  1],
+        'SW': [-1, -1],
+        'SE': [-1,  1],
+    }
 
-    if down > left and down > right:
-        return 'down'
-    elif left > down and left > right:
-        return 'left'
-    else:
-        return 'right'
+    max = -1
+    for direction in directions:
+        pos = directions[direction]
+        current = dir_increment(board, symbol, row, col, pos[0], pos[1])
+        if max < current:
+            max = current
+    return max
 
 def artificial_movement(board):
     max_points = 0
